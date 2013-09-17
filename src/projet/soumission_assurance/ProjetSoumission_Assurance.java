1e11c5bb25e08d985d836a21cd38983fdacaf265
Fred Kacem <foufan@gmail.com>
2013-09-17 01:12:13 -0400

    Test-Création d'un projet JAVA

---
+++ src/projet/soumission_assurance/ProjetSoumission_Assurance.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.soumission_assurance;
+
+/**
+ *
+ * @author account
+ */
+public class ProjetSoumission_Assurance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
